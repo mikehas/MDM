f574bf664a3b1c088544b2c4b0a3e24297903d97
Michael Haskell <mhaskell@csclnx00.ad.calpoly.edu>
2015-04-21 01:02:01 -0700

    added mdm.cnf

--- mysql_test.py
+++ mysql_test.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 '''
-
 MySQLdb Docs:
 http://mysql-python.sourceforge.net/MySQLdb.html
 
@@ -10,23 +9,28 @@ http://mysql-python.sourceforge.net/MySQLdb.html
 import os
 import ConfigParser
 import MySQLdb
+import logging
 
 home = os.path.expanduser("~")
-mycnf = os.path.join(home, 'my.cnf')
 
+mycnf = os.path.join(home, 'my.cnf')
+appcnf = 'mdm.cnf'
+
+# Read ~/my.cnf, then the local mdm.cnf
 conf = ConfigParser.ConfigParser()
 conf.readfp(open(mycnf))
+conf.readfp(open(appcnf))
 
+# Connect to DB
 db = MySQLdb.connect(host = conf.get('mysql','host'),
                      user = conf.get('mysql','user'),
                      passwd = conf.get('mysql', 'password'),
                      db = conf.get('mysql', 'database'))
 
+print "\nBASIC STATEMENT EXAMPLE"
 q_test = 'show tables'
-
 cur = db.cursor()
 
-print "\nBASIC STATEMENT EXAMPLE"
 
 cur.execute(q_test)
 for row in cur.fetchall():
@@ -41,4 +45,7 @@ cur.execute(q_select_all, (campus,))
 
 for row in cur.fetchall():
    print row
+
+print conf.get('app', 'logdir')
+
 
