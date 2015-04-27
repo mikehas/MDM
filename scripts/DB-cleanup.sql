5703820690a79266cbef95c1a2a09391e3645549
Michael Haskell <mikehas@gmail.com>
2015-04-27 16:41:18 -0700

    downloaded lab 3 scripts

---
+++ scripts/DB-cleanup.sql
@@ -0,0 +1,4 @@
+-- The Rolling Pebbles
+-- CPE366-03
+
+drop table MatchedSecondarySpecialities, MatchedPrimarySpecialities, MatchedAddress, MatchedPhone, Matched, MasteredMedicalProvider, Phones, Address, MedicalProvider, RawData, Specialty;
---
+++ scripts/DB-setup.sql
@@ -0,0 +1,113 @@
+-- The Rolling Pebbles
+-- CPE366-03
+
+create table Specialty (
+	Code varchar(10) primary key,
+	Description varchar(255)
+);
+
+create table RawData (
+	SourceID int primary key auto_increment,
+	ProviderType varchar(20) not null,
+	Name varchar(255) not null,
+	Gender char(1),
+	DateOfBirth varchar(255),
+	isSoleProprietor char(1),
+	MailAddress varchar(255),
+	PracticeAddress varchar(255),
+	Phone varchar(255),
+	Speciality varchar(255)
+);
+
+create table MedicalProvider (
+	SourceID int primary key,
+	ProviderType varchar(20) not null,
+	Name varchar(255) not null,
+	Gender char(1),
+	DateOfBirth varchar(255),
+	isSoleProprietor char(1),
+	PrimarySpeciality varchar(10),
+	SecondarySpeciality varchar(10),
+	Timestamp datetime,
+	Message varchar(255),
+	Constraint MP_RD foreign key (SourceID) references RawData(SourceID),
+	Constraint MP_SPP foreign key (PrimarySpeciality) references Specialty(Code),
+	Constraint MP_SPS foreign key (SecondarySpeciality) references Specialty(Code)
+);
+
+create table Address (
+	SourceID int,
+	AddressType varchar(10),
+	Country varchar(255),
+	Region varchar(255),
+	County varchar(255),
+	City varchar(255),
+	PostalCode varchar(255),
+	Unit varchar(255),
+	Primary key (SourceID, AddressType),
+	Constraint A_MP foreign key (SourceID) references MedicalProvider(SourceID)
+);
+
+create table Phones (
+	SourceID int primary key,
+	Country int,
+	Area int,
+	Exchange int,
+	Subscriber int,
+	Ext int,
+	Constraint P_MP foreign key (SourceID) references MedicalProvider(SourceID)
+);
+
+create table MasteredMedicalProvider (
+	MasterID int primary key auto_increment,
+	ProviderType varchar(20) not null,
+	Name varchar(255) not null,
+	Gender char(1),
+	DateOfBirth varchar(255),
+	isSoleProprietor char(1)
+);
+
+create table Matched (
+	SourceID int,
+	MasterID int,
+	Timestamp datetime,
+	MatchRule varchar(255),
+	Message varchar(255),
+	Primary key (SourceID, MasterID),
+	Constraint M_MP foreign key (SourceID) references MedicalProvider(SourceID),
+	Constraint M_MMP foreign key (MasterID) references MasteredMedicalProvider(MasterID)
+);
+
+create table MatchedPhone (
+	MasterID int,
+	SourceID int,
+	Primary key (SourceID, MasterID),
+	Constraint MPH_MP foreign key (SourceID) references MedicalProvider(SourceID),
+	Constraint MPH_MMP foreign key (MasterID) references MasteredMedicalProvider(MasterID)
+);
+
+create table MatchedAddress (
+	MasterID int,
+	SourceID int,
+	AddressType varchar(10),
+	Primary key (MasterID, SourceID, AddressType),
+	Constraint MA_MP foreign key (SourceID) references MedicalProvider(SourceID),
+	Constraint MA_MMP foreign key (SourceID, AddressType) references Address(SourceID, AddressType)
+);
+
+create table MatchedPrimarySpecialities (
+	MasterID int,
+	Speciality varchar(10),
+	Primary key(MasterID, Speciality),
+	Constraint MPS_MMP foreign key (MasterID) references MasteredMedicalProvider(MasterID),
+	Constraint MPS_S foreign key (Speciality) references Specialty(Code)
+);
+
+create table MatchedSecondarySpecialities (
+	MasterID int,
+	Speciality varchar(10),
+	Primary key(MasterID, Speciality),
+	Constraint MSS_MMP foreign key (MasterID) references MasteredMedicalProvider(MasterID),
+	Constraint MSS_S foreign key (Speciality) references Specialty(Code)
+);
+
