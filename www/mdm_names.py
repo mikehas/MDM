e8f5227bd2ef7bbab19388fa390436ed8aa03d47
Michael Haskell <mikehas@gmail.com>
2015-06-08 01:21:51 -0700

    Mastered Names Split working approx 1000 records

--- www/mdm_names.py
+++ www/mdm_names.py
@@ -24,15 +24,24 @@ Medical Cred, First Name, Middle Initial, Last Name, Name Suffix (Jr)
 Ivonne De L Fraga Berrios
 FirstName, First Name, Middle Initial, Last Name, Lastname
 '''
+BAD_CHARS = '[- .,;:/\r\n>]'
 
 def create_dict_from_file(file_name):
   f = open(file_name)
   d = {}
+  clean_rgx = re.compile(BAD_CHARS)
   for w in f.readlines():
-    w = w.strip('-.,;:\r\n').upper()
+    w = clean_rgx.sub('', w).upper()
     d[w] = w
   f.close()
   return d
+
+def delete_used(to_del, s):
+    to_del = sorted(to_del, reverse=True)
+    for i in to_del:
+      del s[i]
+    to_del = []
+    return to_del, s
 
 def split_names(app):
   cred_d = create_dict_from_file("name_terms/credentials.txt")
@@ -42,29 +51,40 @@ def split_names(app):
   suffix_d = create_dict_from_file("name_terms/suffixes.txt")
 
   # Credential Regex to match consonants strings of 3 or more
-  cred_r = re.compile(r'[^ ]([B-DF-HJ-NP-TVXZ]{3,})[ $]')
+  cred_r = re.compile(r'(^[B-DF-HJ-NP-TV-XZ]{3,}$)')
+  cred_r2 = re.compile(r'(?:.*\s|^)(M D|D D S)(?:\s.*|$)')
 
   session = Session()
   data = session.query(MasteredProvider.name).limit(1000)
   session.close()
   
   output = []
+  clean_rgx = re.compile(BAD_CHARS)
 
   for r in data:
-    name = r[0]
-    credential = cred_r.match(name)
-    if credential is not None:
-      row[6] = None
-    
-    s = name.split()
+    name = r[0].strip()
+    name = re.sub(' - ', '-', name)
     credentials = []
+    prefixes = []
     suffixes = []
-
     to_del = []
+    last = []
+    first = []
+    middle = []
+
+    # Deal with M D, D D S
+    name = re.sub(r'[><]', '', name)
+    credential = cred_r2.match(name)
+    if credential != None:
+      c = credential.group(1)
+      name = re.sub(c, '', name)
+      credentials.append(re.sub(' ','', c))
+
+    s = name.split()
     
     # Find credentials, suffix
     for i, w in enumerate(s):
-      w = w.strip('-.,;:/\r\n').upper()
+      w = clean_rgx.sub('', w).upper()
       credential = cred_r.match(w)
       if credential is not None:
         app.logger.debug(credential.group())
@@ -76,21 +96,70 @@ def split_names(app):
       elif suffix_d.get(w) is not None:
         suffixes.append(w)
         to_del.append(i)
+      elif prefix_d.get(w) is not None:
+        prefixes.append(w)
+        to_del.append(i)
+   
+    # delete used words
+    to_del, s = delete_used(to_del, s)
 
-    # delete creds from word
-    to_del = sorted(to_del, reverse=True)
-    for i in to_del:
-      del s[i]
+    # only name sequences remaining
+    for i, w in enumerate(s):
+      w = clean_rgx.sub('', w).upper()
 
-    row = [''] * 7
+      # I is a suffix if it as the end of a name sequence
+      if w == 'I' and i == len(s) - 1:
+        suffixes.append(w)
+        to_del.append(i)
+
+    # delete used words
+    to_del, s = delete_used(to_del, s)
+
+    # Deal with 2 word names
+    if len(s) == 2:
+      if len(s[0]) == 1:
+        first.append(s[0])
+        last.append(s[1])
+      elif len(s[1]) == 1:
+        middle.append(s[1])
+        last.append(s[0])
+      else:
+        first.append(s[0])
+        last.append(s[1])
+      to_del.extend([0,1])
+
+    # Deal with 3 word names
+    elif len(s) == 3:
+      first.append(s[0])
+      middle.append(s[1])
+      last.append(s[2])
+      to_del.extend([0,1,2])
+
+    # Deal with 4 word names
+    elif len(s) == 4:
+      if len(s[2]) == 1:
+        first.extend([s[0], s[1]])
+        middle.append(s[2])
+        last.append(s[3])
+        to_del.extend([0,1,2,3])
+      elif len(s[1]) == 1:
+        first.append(s[0])
+        middle.append(s[1])
+        last.extend([s[2],s[3]])
+        to_del.extend([0,1,2,3])
+
+    to_del, s = delete_used(to_del, s)
+
+    row = [''] * 8
     row[0] = r[0]
-    #row[1] = None
-    row[2] = ' '.join(s)
-    #row[3] = None
-    row[4] = ' '.join(suffixes)
-    row[5] = ' '.join(credentials)
+    row[1] = ' '.join(prefixes)
+    row[2] = ' '.join(first)
+    row[3] = ' '.join(middle)
+    row[4] = ' '.join(last)
+    row[5] = ' '.join(suffixes)
+    row[6] = ' '.join(credentials)
+    row[7] = ' '.join(s)
     output.append(row)
 
   return output
-
 
