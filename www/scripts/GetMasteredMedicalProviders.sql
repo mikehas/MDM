38ba37c490f6b3cd62ed3595683cf84c5ce6a38f
jiwu14 <sjiwu14@gmail.com>
2015-06-09 19:06:03 -0700

    Delete GetMasteredMedicalProviders.sql

--- www/scripts/GetMasteredMedicalProviders.sql
+++
@@ -1,6 +0,0 @@
-select mmp.*,
-   (select CleanPhone from Phones ph, MatchedPhone mph where ph.sourceid = mph.sourceid and mph.masterid = mmp.masterid limit 1) as Phone,
-   (select Specialty from MatchedPrimarySpecialties mps where mps.masterid = mmp.masterid limit 1) as PrimarySpecialty,
-   (select Specialty from MatchedSecondarySpecialties mss where mss.masterid = mmp.masterid limit 1) as SecondarySpecialty
-from MasteredMedicalProvider mmp;
-
